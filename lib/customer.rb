a492d314fab9e8551f73544b146b116834bddb42
Simon51584 <simon51584@gmail.com>
2020-07-08 21:55:12 -0500

    seths update esrb & genre

--- lib/customer.rb
+++ lib/customer.rb
@@ -2,6 +2,24 @@ class Customer < ActiveRecord::Base
     has_many :videogames   
     has_many :developers, through: :videogames
 
+    # def self.customers_all
+    #     self.Customers.all
+    # end
+    #as a customer I want to select all the genres
+    #as a customer I want to 
+
+    # def self.genre
+    #     self.all.select do |t|
+    #         t.videogames
+    #     end
+    # end 
+
+    # def video_games
+    #     self.videogames.count
+    # end 
+
+    
+
 
 
 end
